9e64b5aa66aa0636248927f12770555fa9be226d
nanjizal <none>
2019-03-04 23:09:05 +0000

    add Variants, ArrayVariant abstract

---
+++ src/hxTShopify/t/ArrayVariant.hx
@@ -0,0 +1,57 @@
+package hxTShopify.t;
+import hxTShopify.t.Variant;
+// used as a helper for working with Variants
+@:forward
+abstract VariantArray( Array<Variant> ) to Array<Variant> {
+    public inline function new( variants: Null<Array<Variant>> ){
+        this = ( variants == null )? empty(): variants;
+    }
+    static inline function empty(){
+        return new VariantArray( new Array<Variant>() );
+    }
+    public function stringQuanitiesBelow( max: Int ){
+        var variant: Variant;
+        var str: String = '';
+        for( i in 0...this.length ){
+            var variant = this[ i ];
+            if( variant.inventory_quantity < max ) str += variant.title + '( ' + variant.inventory_quantity + ' ),\n ';
+        }
+        return str;
+    }
+    public function quanitiesBelow( max: Int ){
+        var variant: Variant;
+        var variants = new VariantArray();
+        for( i in 0...this.length ){
+            var variant = this[ i ];
+            if( variant.inventory_quantity < max )  variants[ variants.length ] = variant;
+        }
+        return varants;
+    }
+    public function stringQuanitiesAbove( min: Int ){
+        var variant: Variant;
+        var str: String = '';
+        for( i in 0...this.length ){
+            var variant = this[ i ];
+            if( variant.inventory_quantity > min ) str += variant.title + '( ' + variant.inventory_quantity + ' ),\n ';
+        }
+        return str;
+    }
+    public function quanitiesBelow( min: Int ){
+        var variant: Variant;
+        var variants = new VariantArray();
+        for( i in 0...this.length ){
+            var variant = this[ i ];
+            if( variant.inventory_quantity > min )  variants[ variants.length ] = variant;;
+        }
+        return varants;
+    }
+    public function stringQuanities(){
+        var variant: Variant;
+        var strTemp: String = '';
+        for( i in 0...this.length ){
+            var variant = this[ i ];
+            str += variant.title + '( ' + variant.inventory_quantity + ' ),\n ';
+        }
+        return str;
+    }
+}
