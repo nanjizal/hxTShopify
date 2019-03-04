c2a32af754405439725a244bc3440a8eb848a406
nanjizal <none>
2019-03-04 20:16:48 +0000

    adjustments for forcing ids to strings

--- src/hxTShopify/t/ProductSerializer.hx
+++ src/hxTShopify/t/ProductSerializer.hx
@@ -4,6 +4,6 @@ import hxbit.Serializable;
 class ProductSerializer implements hxbit.Serializable {
     @:s public var product : Product;
     public function new( product_ ){
-        product = product_;
+        product = ( product_: ProductWrapper );
     }
 }
