2cf566fd89bebf2afce076c34bcab4e3fb163ec2
nanjizal <none>
2019-03-04 10:36:15 +0000

    _

--- src/hxTShopify/api/Transfer.hx
+++ src/hxTShopify/api/Transfer.hx
@@ -53,4 +53,45 @@ class Transfer{
                       );
         });
     }
+    public static function productUpdate( from_:                 Shop
+                                        , to_:                   Shop
+                                        , searchString:          String
+                                        , productTransformation: Product->Product
+                                        , variantTransformation: (Variant,Variant)->Variant
+                                        , loaded:                ( Product, String )->Void
+                                        , log:                   String->Void
+                                         ){
+        Search.log = log;
+        Search.searchProducts( from_, searchString, 
+        function( ps: Array<Product> ){
+            // exit if no product or multiple product found
+            if( ps.length != 1 ){
+                if( ps.length == 0 || ps.length == null ){
+                    log( 'error: no product found for: $searchString' );
+                } else {
+                    log( 'error: lots of products found for: $searchString + ( ${ps.length} )' );
+                }
+                return;
+            }
+            var p                      = ps[ 0 ];
+            loaded( p.title, searchString );
+            
+            /*
+            var https = new Https<ProductHolder>();
+            https.onLoad = function( ps_: ProductHolder ){
+                var p_: Product = ps_.product;
+                loaded( p_, searchString );
+            }
+            https.onError              = ( e: String ) -> log( 'error loading $searchString $e' );
+            var p                      = ps[ 0 ];
+            var productClone: Product  = ProductCloner.withChanges( p, productTransformation, variantTransformation );
+            var payload: ProductHolder = { product: productClone };
+            https.put( to_.constructPath( PRODUCT )
+                      , payload
+                      , to_.getKeyPass() 
+                      );
+            */
+        });
+    }
+    
 }
