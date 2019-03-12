02794c6c9da36ff6d8c837be9fe93d2c0bf916b8
nanjizal <nanjizal@users.noreply.github.com>
2019-03-12 11:48:55 +0000

    Update Variant.hx

--- src/hxTShopify/t/Variant.hx
+++ src/hxTShopify/t/Variant.hx
@@ -15,7 +15,8 @@ typedef Variant = {
     var inventory_policy:       String;
     @:optional var compare_at_price:       Float;
     var fulfillment_service:    String;
-    var inventory_management:   String;
+    // do not try to create new product with this set, leaving out sets to shopify, null to do not track
+    @:optional var inventory_management:   String;
     var option1:                Null<String>;
     var option2:                Null<String>;
     var option3:                Null<String>;
